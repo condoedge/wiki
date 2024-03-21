142c543b9e9cbc5dcede3809929bf3c28293e22c
basslhm <contact@kompo.io>
2024-03-21 17:39:24 -0300

    remove helper not generalized

--- src/Components/ArticleRawList.php
+++ src/Components/ArticleRawList.php
@@ -23,7 +23,7 @@ class ArticleRawList extends Table
 	{
 		return _Rows(
 			_Flexbetween(
-				_H1('wiki.articles')->medTitle()->class('text-level3'),
+				_H1('wiki.articles')->class('text-level3'),
 				_Link('wiki.create-article')->button()->icon('icon-plus')->href('knowledge.editor'),
 			),
             _FlexEnd(
